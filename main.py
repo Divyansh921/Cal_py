9b8ab0b7e892e1cf641b583d7b75475183759209
Divyansh Kumar <divyanshkumarsaini@hotmial.com>
2026-07-05 21:55:32 +0530

    Added more features

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #A calculator for basic calculation
 a = float(input("Enter first digit"))
 b = float(input("enter second digit"))
-operation = str(input("enter the operation +,-,*,/"))
+operation = str(input("enter the operation +,-,*,%,/,//,**"))
 
 def calculator(a,b,operation):
     match operation:
@@ -11,6 +11,15 @@ def calculator(a,b,operation):
             return a-b
         case "*":
             return a*b
+        case "%":
+            return "reminder is " + str(a%b)
+        case "a**b":
+            return "a raised to the power of b is " + str(a**b)
+        case "a//b":
+            if b == 0:
+                return "Error: Division by zero"
+            else:
+                return "floor division is " + str(a//b)
         case "/":
             if b == 0:
                 return "Error: Division by zero"
